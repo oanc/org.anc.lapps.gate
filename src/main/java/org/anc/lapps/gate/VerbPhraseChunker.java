91346ca60ece8c1287d1819499be0bf489f9901e
Keith Suderman <suderman@anc.org>
2014-04-18 15:32:39 -0500

    Added execute method to the VerbPhraseChunker

--- src/main/java/org/anc/lapps/gate/VerbPhraseChunker.java
+++ src/main/java/org/anc/lapps/gate/VerbPhraseChunker.java
@@ -1,6 +1,12 @@
 package org.anc.lapps.gate;
 
+import gate.Document;
+import gate.Factory;
+import gate.FeatureMap;
+import org.lappsgrid.api.Data;
+import org.lappsgrid.core.DataFactory;
 import org.lappsgrid.discriminator.Types;
+import org.lappsgrid.vocabulary.Contents;
 
 /**
  * @author Keith Suderman
@@ -22,4 +28,27 @@ public class VerbPhraseChunker extends PooledGateService
    {
       return new long[] { Types.GATE, Types.TOKEN, Types.POS };
    }
+
+   public Data execute(Data input)
+   {
+      Document document = null;
+      try
+      {
+         document = doExecute(input);
+      }
+      catch (Exception e)
+      {
+         return DataFactory.error("Unable to execute the Coreferencer.", e);
+      }
+      if (document == null)
+      {
+         return DataFactory.error(BUSY);
+      }
+      String producer = this.getClass().getName() + "_" + Version.getVersion();
+      FeatureMap features = Factory.newFeatureMap();
+      features.put("type", Contents.Chunks.VERBS);
+      features.put("producer", producer);
+      return DataFactory.gateDocument(document.toXml());
+   }
+
 }
